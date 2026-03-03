c7f86a6bae661f4364bc2a2b0e3fb3a0bc2043ca
gudtjq0508 <gudtjq0508@gmail.com>
2026-03-03 07:24:40 +0000

    Update core project files: index.html, main.js, and style.css

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// Add JS here
+const generatorBtn = document.getElementById('generator-btn');
+const numbersContainer = document.getElementById('numbers-container');
+
+generatorBtn.addEventListener('click', () => {
+  numbersContainer.innerHTML = '';
+  const numbers = [];
+  while (numbers.length < 6) {
+    const number = Math.floor(Math.random() * 45) + 1;
+    if (!numbers.includes(number)) {
+      numbers.push(number);
+    }
+  }
+  numbers.sort((a, b) => a - b);
+  numbers.forEach(number => {
+    const numberElement = document.createElement('div');
+    numberElement.classList.add('number');
+    numberElement.textContent = number;
+    numbersContainer.appendChild(numberElement);
+  });
+});
